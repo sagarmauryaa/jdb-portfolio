70e1bb81ac36ffa8c5e8000fbe9ddc6ed14d64ee
Sagar Maurya <sagar68742@gmail.com>
2025-09-14 14:02:22 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    minimumCacheTTL: 604800,
+    formats: ["image/webp", "image/avif"],
+  },
+  output: "export", 
 };
 
 export default nextConfig;
